59f81b0384e2d1a3dc9ec3c54c99f279dbb51caf
bzh <zhangbin_xjtu@hotmail.com>
2018-01-24 23:09:54 +0100

    Finish section 02_07 but with issues of plotly's polar charts, documented in app.py

--- app.py
+++ app.py
@@ -6,44 +6,81 @@ import plotly.graph_objs as go
 
 import numpy as np
 
-from lectures_code.section_02 import z
+from lectures_code.section_02 import x, k1, euler1
 
 app = dash.Dash()
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
+
+# due to issues of plotly polar charts, this work is suspended for now
+# see https://github.com/plotly/plotly.js/issues/275
+
 app.layout = html.Div([
-    html.H1("The complexe number z = 4 + 3i"),
     dcc.Graph(
-        id='single complex number',
+        id='exponential',
         figure={
             'data': [
                 go.Scatter(
-                    x=np.array([z.real]),
-                    y=np.array([z.imag]),
-                    # x=random_x,
-                    # y=random_y,
-                    text='complexe number (4 + 3i)',
-                    mode='markers',
+                    x=x,
+                    y=np.exp(x),
+                    mode='lines',
                     marker={
-                        'size': 15,
-                        'color': 'red',
-                        'line': {'width': 0.5, 'color': 'white'}
+                        'color': 'blue'
                     },
-                    name='complexe number'
+                    name='exponetial curve'
                 )
             ],
 
             'layout': go.Layout(
-                title='Complex numbers',
-                xaxis={'title': 'Real axis',
-                       'range': [-5, 5]},
-                yaxis={'title': 'Imaginary axis',
-                       'range': [-5, 5]},
+                title='Exponetial Curve',
+                titlefont={"size": 16,
+                           "color": "red"},
+                xaxis={'title': 'x',
+                       'range': [np.min(x), np.max(x)]},
+                yaxis={'title': 'exp(x)',
+                       'range': [0, np.exp(x[-1])]},
                 width=700,
                 height=700
             )
-        }
+        },
+        style={'display': 'inline-block'}
+    ),
+
+    dcc.Graph(
+        id='complex number plot',
+        figure={
+            'data': [
+                go.Scatter(
+                    r=np.array([np.abs(euler1)]),
+                    t=np.array([np.angle(euler1)*180/np.pi]),
+                    mode='markers',
+                    name='Trial 4',
+                    marker=dict(
+                        color='rgb(231,41,138)',
+                        size=200,
+                        line=dict(
+                            color='red'
+                        ),
+                        opacity=0.7
+                    )
+                )
+            ],
+
+            'layout': go.Layout(
+                title='Complex Number Plot',
+                titlefont={"size": 16,
+                           "color": "red"},
+                font={'size': 15},
+                plot_bgcolor='rgb(223,223,223)',
+                angularaxis={'tickcolor': 'rgb(253,253,253)'},
+                direction='counterclockwise',
+                width=700,
+                height=700
+            )
+        },
+        style={'display': 'inline-block'}
     )
+
 ])
 
 if __name__ == '__main__':
--- lectures_code/section_02.py
+++ lectures_code/section_02.py
@@ -1,15 +1,54 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# create a complexe number with 1j
+# -----section 02_06: complex numbers-----
+
+# create a complex number with 1j
 z = 4 + 1j * 3
 
-# extract the real and imaginary parts of the complexe number
-z_real = np.real(z) # or z.real
-z_imag = np.imag(z) # or z.imag
+# extract the real and imaginary parts of the complex number
+z_real = np.real(z)  # or z.real
+z_imag = np.imag(z)  # or z.imag
 
-# calculate the magnitude of a complexe number
+# calculate the magnitude of a complex number
 z_mag = np.abs(z)
 
-# calculate the angle of the complexe number
+# calculate the angle of the complex number
 z_angle = np.angle(z)
+
+# -----section 02_07: Euler's formula e^ik = cos(k) + isin(k)-----
+
+# exp() curve
+x = np.linspace(-3, 3, num=50)
+plt.plot(x, np.exp(x), label='y=e^x')
+plt.axis([min(x), max(x), 0, np.exp(x[-1])])
+plt.grid(True)
+plt.legend()
+plt.xlabel('x')
+
+# cos(k) and sin(k) on the unit circle
+
+k1 = np.pi / 3
+k2 = np.pi / 2
+euler1 = np.exp(1j*k1)
+euler2 = np.exp(1j*k2)
+x = np.linspace(-np.pi, np.pi, num=100)
+
+plt.plot(np.cos(x), np.sin(x))
+
+# two method to get the real and imaginary parts of e^ik
+plt.plot(np.cos(k1), np.sin(k1), 'ro')
+plt.plot(euler2.real, euler2.imag, 'ro')
+
+plt.axis('square')
+plt.grid(True)
+plt.xlabel('Real axis'), plt.ylabel('Imaginary axis')
+
+m = 20
+compnum = m * euler1
+
+mag = np.abs(compnum)
+phs = np.angle(compnum)
+
+plt.polar([phs, phs], [0, mag])
 
